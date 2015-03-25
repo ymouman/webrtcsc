b3f8f830910bcafa7fabf51d7609a38f50251de9
Younes Mouman <younes.mouman@gmail.com>
2015-03-25 06:22:40 +0100

    first

---
+++ README.md
@@ -0,0 +1,71 @@
+webrtc-application-screen-share    
+===============================
+
+Capture the users screen or application and share over a WebRTC PeerConnection. This simple demo consists of a presenter who shares their screen or applications and an attendee who views that share video on a web page.  Audio and video will also be transmitted between both peers. The attendee can be either a Chrome or Firefox browser. The presenter must be using Chrome.  So right now this is a two user demo, one presentor and one attendee only.
+
+This repo has three pieces.
+
+1. Chrome packaged app to share screen applications and audio\video. Can be used both peers being presentor or attendee depending who shares first.
+2. Server application using node.js and web socket to relay messages between peers.
+3. Attendee: Web page that receives and displays the shared video stream (Chrome or Firefox).
+
+
+- Presenter and Attendee packaged apps only work using Google Chrome, (Stable, Canary or Chromium).
+- No need for a Web Server, it uses node.js which does both the WebRTC signaling and serves up the presentor web page.
+
+![Architecture diagram](https://github.com/emannion/webrtc-application-screen-share/blob/master/arch.png "Arch diagram")
+
+#### screen or application capture uses getUserMedia API and chrome.desktopCapture
+
+```javascript
+
+function onAccessApproved(id) {
+  if (!id) {
+    console.log("Access rejected");
+    return;
+  }
+  navigator.webkitGetUserMedia({                                                                                                                                                                                                              
+      audio: false,
+      video: { mandatory: { chromeMediaSource: "desktop",
+                            chromeMediaSourceId: id, 
+                            maxWidth: screen.width,
+                            maxHeight: screen.height,
+                            minFrameRate: 1,
+                            maxFrameRate: 5 }}
+  }, gotStream, getUserMediaError);
+}
+
+document.querySelector('#share').addEventListener('click', function(e) {
+  pending_request_id = chrome.desktopCapture.chooseDesktopMedia(
+      ["screen", "window"], onAccessApproved);
+});
+
+
+```
+
+####  Setup prerequesites
+
+- Install Node.js  and  websocket (sudo npm install websocket)
+
+####  Server Steps (Tested on Linux, MacOS and Windows)
+
+- clone this repo to your machine, does not need to be to a web server.
+- Edit packaged_app/app.html (insert this machines ip address for WebSocket connection). Or use configure button when running.
+- run 'node app.js'  or 'sudo node app.js' depending on your user, try both.
+
+####  Client Packaged App steps (Chrome browser or Chromebook)
+
+- Install packaged app in chrome
+- i.e. open 'chrome://extensions'
+- click 'Developer Mode' check box.
+- Click button 'Load unpacked extension'
+- Navigate to the packaged_app folder of this repo
+- Launch app by clicking 'launch' link in chrome://extensions
+-  Or use Chrome App Launcher to launch it.
+
+####  Client Attendee Web Page Steps 
+
+- Start Chrome or Firefox browser
+- Point browser to  e.g. http://\<your node.js ip address\>:1337
+- You can test this on the same machine or across the network over two machines.
+
---
+++ packaged_app/app.js
@@ -0,0 +1,430 @@
+// Replace with your server domain or ip address, or use configure button on app to set this
+var serverAddress = '127.0.1.1' ;
+var socket = null;
+var shareVideo = null;
+var localVideo = null;
+var remoteVideo = null;
+var shareStream = null;
+var videoStream = null;
+var shareFlowing = false;
+var videoFlowing = false;
+var isPresentor = false;
+var shareVideoActive = false;
+var remoteVideoActive = false;
+var removeVP8Codec = false;
+var joinReceived = false;
+var pending_request_id = null;
+var pconns = {};
+
+var mediaConstraints = {'mandatory': {
+                        'OfferToReceiveAudio':true, 
+                        'OfferToReceiveVideo':true}};
+
+shareVideo = document.getElementById("shareVideo");
+
+var serverString = 'ws://' + serverAddress + ':1337';                                                                                                                                                                                       
+socket = new WebSocket(serverString);
+socket.addEventListener("message", onWebSocketMessage, false);
+
+function gotShareStream(stream) {
+  shareVideo.src = URL.createObjectURL(stream);
+  shareStream = stream;
+  share();
+
+  if ("WebSocket" in window) {
+    socket.onopen = function() {
+      console.log("WebSocket connection open");
+    };
+  } else {
+    console.log("No web socket connection");
+  }
+
+  stream.onended = function() { console.log("Share stream ended"); };
+}
+
+function gotAudioVideoStream(stream) {
+  localVideo = document.getElementById("localVideo");
+  remoteVideo = document.getElementById("remoteVideo");  // may move this
+  localVideo.src = URL.createObjectURL(stream);
+  videoStream = stream;
+  if (isPresentor) {
+    connect();
+  }
+  stream.onended = function() { console.log("Audio Video stream ended"); };
+}
+
+function errorCallback(error) {
+  console.error('An error occurred: [CODE ' + error + ']');
+  return;
+}
+
+function onAccessApproved(id) {
+  isPresentor = true;
+  if (!id) {
+    console.log("Access rejected.");
+    return;
+  }
+  navigator.webkitGetUserMedia({
+      audio: false,
+      video: { mandatory: { chromeMediaSource: "desktop",
+                            chromeMediaSourceId: id,
+                            maxWidth: screen.width,
+                            maxHeight: screen.height,
+                            minFrameRate: 1,
+                            maxFrameRate: 5 }}
+  }, gotShareStream, errorCallback);
+}
+
+document.querySelector('#share').addEventListener('click', function(e) {
+  isPresentor = true;
+  joinReceived = false;
+
+  // turn on audio \ video on attendee
+  socket.send(JSON.stringify({
+                "pc": 0,
+                "messageType": "publish"
+             }));
+});
+
+document.querySelector('#cancel').addEventListener('click', function(e) {
+  if (pending_request_id != null) {
+    chrome.desktopCapture.cancelChooseDesktopMedia(pending_request_id);
+  }
+  disconnect();
+});
+
+document.querySelector('#configure').addEventListener('click', function(e) {
+  var overlay = document.getElementById("overlay");
+  var popup = document.getElementById("popup");
+  overlay.style.display = "block";
+  popup.style.display = "block";
+  document.getElementById("serverAddress").value = serverAddress;
+});
+
+document.querySelector('#closeConfiguration').addEventListener('click', function(e) {
+  joinReceived = false; 
+  var overlay = document.getElementById("overlay");
+  var popup = document.getElementById("popup");
+  overlay.style.display = "none";
+  popup.style.display = "none"; 
+  removeVP8Codec = document.getElementById('h264').checked;
+
+  if (document.getElementById('small').checked) {
+    document.getElementById("remoteVideo").className = "video-small";
+  } else if (document.getElementById('medium').checked) {
+    document.getElementById("remoteVideo").className = "video-medium";
+  } else {
+    document.getElementById("remoteVideo").className = "video-large";
+  }
+
+  serverAddress = document.getElementById("serverAddress").value;
+  serverString = 'ws://' + serverAddress + ':1337';                                                                                                                                                                                       
+  socket = new WebSocket(serverString);
+  socket.addEventListener("message", onWebSocketMessage, false);
+});
+
+function raiseMeetingNotification() {
+  var overlay = document.getElementById("overlayMedia");
+  var popup = document.getElementById("popupMedia");
+  overlay.style.display = "block";
+  popup.style.display = "block";
+}
+
+document.querySelector('#joinMeeting').addEventListener('click', function(e) {
+  startVideo();
+
+  socket.send(JSON.stringify({
+                "pc": 0,
+                "messageType": "join"
+             }));
+
+  closeMeetingNotification();
+});
+
+document.querySelector('#cancelMeeting').addEventListener('click', function(e) {
+  closeMeetingNotification();
+});
+
+function closeMeetingNotification() {
+  var overlay = document.getElementById("overlayMedia");
+  var popup = document.getElementById("popupMedia");
+  overlay.style.display = "none";
+  popup.style.display = "none"; 
+}
+
+function removeVP8(sdp) {
+  console.log("SDP before manipulation: " + sdp);
+  //updated_sdp = sdp.replace("m=video 1 RTP/SAVPF 100 116 117 96 120 121\r\n","m=video 1 RTP/SAVPF 120 121\r\n");
+  //updated_sdp = sdp.replace("m=video 1 RTP/SAVPF 100 116 117 96 120 121\r\n","m=video 1 RTP/SAVPF 120\r\n");
+  updated_sdp = sdp.replace("m=video 9 RTP/SAVPF 100 116 117 120 96\r\n","m=video 9 RTP/SAVPF 120\r\n");
+  updated_sdp = updated_sdp.replace("","");
+  updated_sdp = updated_sdp.replace("a=rtpmap:100 VP8/90000\r\n","");
+  updated_sdp = updated_sdp.replace("a=rtpmap:120 H264/90000\r\n","a=rtpmap:120 H264/90000\r\na=fmtp:120 profile-level-id=42e01f;packetization-mode=1\r\n");
+
+  updated_sdp = updated_sdp.replace("a=rtcp-fb:100 nack\r\n","");
+  updated_sdp = updated_sdp.replace("a=rtcp-fb:100 nack pli\r\n","");
+  updated_sdp = updated_sdp.replace("a=rtcp-fb:100 ccm fir\r\n","");
+  updated_sdp = updated_sdp.replace("a=rtcp-fb:100 goog-remb\r\n","");
+  updated_sdp = updated_sdp.replace("a=rtpmap:116 red/90000\r\n","");
+  updated_sdp = updated_sdp.replace("a=rtpmap:117 ulpfec/90000\r\n","");
+  updated_sdp = updated_sdp.replace("a=rtpmap:96 rtx/90000\r\n","");
+  updated_sdp = updated_sdp.replace("a=fmtp:96 apt=100\r\n","");
+
+  updated_sdp = updated_sdp.replace("a=rtpmap:121 CAST1/90000\r\n","");
+  updated_sdp = updated_sdp.replace("a=rtcp-fb:121 ccm fir\r\n","");
+  updated_sdp = updated_sdp.replace("a=rtcp-fb:121 nack\r\n","");
+  updated_sdp = updated_sdp.replace("a=rtcp-fb:121 nack pli\r\n","");
+  updated_sdp = updated_sdp.replace("a=rtcp-fb:121 goog-remb\r\n","");
+  
+  console.log("SDP after manipulation: " + updated_sdp);
+
+  return updated_sdp;
+}
+
+// Two peerconnections are used for Firefox compatability, this is
+// because Chrome can do share and video using one PeerConnection but FF needs two.
+function setLocalDescAndSendMessagePC0Offer(sessionDescription) {
+  pconns[0].setLocalDescription(sessionDescription);
+  console.log("Sending: SDP");
+  console.log(sessionDescription);
+  socket.send(JSON.stringify({
+                    "pc": 0,
+                    "messageType": "offer",
+                    "peerDescription": sessionDescription
+              }));
+}
+
+function setLocalDescAndSendMessagePC1Offer(sessionDescription) {
+  pconns[1].setLocalDescription(sessionDescription);
+  console.log("Sending: SDP");
+  console.log(sessionDescription);
+  socket.send(JSON.stringify({
+                    "pc": 1,
+                    "messageType": "offer",
+                    "peerDescription": sessionDescription
+              }));
+}
+
+function setLocalDescAndSendMessagePC0Answer(sessionDescription) {
+  pconns[0].setLocalDescription(sessionDescription);
+  console.log("Sending: SDP");
+  console.log(sessionDescription);
+  socket.send(JSON.stringify({
+                    "pc": 0,
+                    "messageType": "answer",
+                    "peerDescription": sessionDescription
+              }));
+}
+
+function setLocalDescAndSendMessagePC1Answer(sessionDescription) {
+  pconns[1].setLocalDescription(sessionDescription);
+  console.log("Sending: SDP");
+  console.log(sessionDescription);
+  socket.send(JSON.stringify({
+                    "pc": 1,
+                    "messageType": "answer",
+                    "peerDescription": sessionDescription
+              }));
+}
+
+function share() {
+  if (shareStream) {
+    if (!pconns[0]) {
+      createPeerConnection(0);
+    }
+    console.log('Adding local stream...');
+    pconns[0].addStream(shareStream);
+    shareFlowing = true;
+
+    pconns[0].createOffer(setLocalDescAndSendMessagePC0Offer, errorCallback, mediaConstraints);
+
+    startVideo();
+
+  } else {
+    console.log("Local share stream not running.");
+  }
+}
+
+function startVideo() {
+    // grab camera and mic
+    navigator.webkitGetUserMedia({
+      audio: true,
+      video: true
+    }, gotAudioVideoStream, errorCallback);
+}
+
+function connect() {
+  if (!videoFlowing && videoStream) {
+    if (!pconns[1]) {
+      createPeerConnection(1);
+    }
+    console.log('Adding local stream...');
+    pconns[1].addStream(videoStream);
+    videoFlowing = true;
+    pconns[1].createOffer(setLocalDescAndSendMessagePC1Offer, errorCallback, mediaConstraints);
+  } else {
+    console.log("Local stream not running.");
+  }
+}
+
+// stop the connection on button click 
+function disconnect() {
+  console.log("disconnect()");    
+  socket.send(JSON.stringify({
+                "pc": 0,
+                "messageType": "bye"
+             }));
+  stop();
+}
+
+function stop() {
+
+  if (pconns[0] != null) {
+    pconns[0].close();
+    pconns[0] = null;
+  }
+  if (pconns[1] != null) {
+    pconns[1].close();
+    pconns[1] = null;
+  }
+  if (videoStream) {
+    videoStream.stop(); 
+    videoStream = null;
+    localVideo.src = "";
+    remoteVideo.src = "";
+  }
+  if (shareStream) {
+    shareStream.stop();
+    shareStream = null;
+  }
+  if (shareVideo) {
+    shareVideo.src = "";
+  } 
+  shareFlowing = false;
+  videoFlowing = false;
+  isPresentor = false;
+  shareVideoActive = false;
+  remoteVideoActive = false;
+  joinReceived = false;
+}
+
+// process messages from web socket
+function onWebSocketMessage(evt) {
+  var message = JSON.parse(evt.data);
+  var pcID = message.pc;
+
+  if(message.messageType === "offer") {
+
+    console.log("Received offer...")
+    if (!pconns[pcID]) {
+      createPeerConnection(pcID);
+    }
+    shareFlowing = true;
+    console.log('Creating remote session description...' );
+
+    var remoteDescription = message.peerDescription;
+
+    if (removeVP8Codec) {
+      // Remove VP8 from offer
+      remoteDescription.sdp = removeVP8(remoteDescription.sdp);
+    }
+
+    var RTCSessionDescription = window.mozRTCSessionDescription || window.webkitRTCSessionDescription || window.RTCSessionDescription;
+    pconns[pcID].setRemoteDescription(new RTCSessionDescription(remoteDescription), function() {
+      console.log('Sending answer...');
+      if (pcID == 0)
+        pconns[0].createAnswer(setLocalDescAndSendMessagePC0Answer, errorCallback, mediaConstraints);
+      else
+        pconns[1].createAnswer(setLocalDescAndSendMessagePC1Answer, errorCallback, mediaConstraints);
+    }, errorCallback);
+
+  } else if (message.messageType === "answer" && shareFlowing) {
+    var remoteDescription = message.peerDescription;
+    console.log(remoteDescription);
+    console.log('Received answer...');
+    console.log('Setting remote session description...' );
+    var RTCSessionDescription = window.mozRTCSessionDescription || window.webkitRTCSessionDescription || window.RTCSessionDescription;
+    pconns[pcID].setRemoteDescription(new RTCSessionDescription(remoteDescription));
+
+  } else if (message.messageType === "iceCandidate" && shareFlowing) {
+    console.log('Received ICE candidate...');
+    var candidate = new RTCIceCandidate({sdpMLineIndex:message.candidate.sdpMLineIndex, sdpMid:message.candidate.sdpMid, candidate:message.candidate.candidate});
+    console.log(candidate);
+    pconns[pcID].addIceCandidate(candidate);
+
+  } else if (message.messageType === "bye" && shareFlowing) {
+    console.log("Received bye");
+    stop();
+
+  } else if (message.messageType === "publish" ) {
+    console.log("Received publish");
+    if (!isPresentor) {
+      raiseMeetingNotification();
+    }
+
+  } else if (message.messageType === "join" ) {
+    console.log("Received join");
+    if (isPresentor && !joinReceived) {
+      joinReceived = true;
+      pending_request_id = chrome.desktopCapture.chooseDesktopMedia(
+        ["screen", "window"], onAccessApproved);
+    }
+  }
+}
+
+function createPeerConnection(pcID) {
+  console.log("Creating peer connection");
+  RTCPeerConnection = window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+  var pc_config = {"iceServers":[]};
+  try {
+    pconns[pcID] = new RTCPeerConnection(pc_config);
+  } catch (e) {
+    console.log("Failed to create PeerConnection, exception: " + e.message);
+  }
+
+  // send any ice candidates to the other peer
+  pconns[pcID].onicecandidate = function (evt) {
+    if (evt.candidate) {
+      console.log('Sending ICE candidate...');
+      console.log(evt.candidate);
+
+      socket.send(JSON.stringify({
+                    "pc": pcID,
+                    "messageType": "iceCandidate",
+                    "candidate": evt.candidate
+                  }));   
+    } else {
+      console.log("End of candidates.");
+    }
+  };
+
+  console.log('Adding local stream...');
+  if (!isPresentor) {
+    pconns[pcID].addStream(videoStream);
+  }
+
+  pconns[pcID].addEventListener("addstream", onRemoteStreamAdded, false);
+  pconns[pcID].addEventListener("removestream", onRemoteStreamRemoved, false)
+
+  function onRemoteStreamAdded(event) {
+    console.log("Added remote stream");
+
+    if (!shareVideoActive && !isPresentor) {
+      shareVideo.src = window.URL.createObjectURL(event.stream);
+      shareVideo.play();
+      shareVideoActive = true;
+       return;
+    }   
+
+    if (!remoteVideoActive) {
+      remoteVideo.src = window.URL.createObjectURL(event.stream);
+      remoteVideo.play();
+      remoteVideoActive = true;
+    }
+  }
+
+  function onRemoteStreamRemoved(event) {
+    console.log("Remove remote stream");
+    remoteVideo.src = "";
+  }
+}
